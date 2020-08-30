5e5a6dd078309b948b46a763992514b77eec1c24
samshenhong <61616269+samshenhong@users.noreply.github.com>
2020-08-30 10:14:42 +0800

    Add files via upload

---
+++ git/repository/tank/src/com/mashibing/tank/Main.java
@@ -0,0 +1,16 @@
+package com.mashibing.tank;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TankFrame tf = new TankFrame();			
+		
+
+	}
+
+}
